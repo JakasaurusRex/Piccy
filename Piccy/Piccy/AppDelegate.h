cccdc9572b16069f7ba9389f259a853c636cefe5
Jake Torres <jakert2244@gmail.com>
2022-07-05 14:28:08 -0700

    Initial Commit

---
+++ Piccy/Piccy/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Piccy
+//
+//  Created by Jake Torres on 7/5/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Piccy/Piccy/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  Piccy
+//
+//  Created by Jake Torres on 7/5/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
