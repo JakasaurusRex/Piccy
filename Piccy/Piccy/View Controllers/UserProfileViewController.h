c20fcc387100ac41228c36693e590c36ccd25500
Jake Torres <jakert2244@gmail.com>
2022-07-06 14:57:55 -0700

    Added Home and Profile view Controllers and Navigating between them

---
+++ Piccy/Piccy/View Controllers/UserProfileViewController.h
@@ -0,0 +1,16 @@
+//
+//  UserProfileViewController.h
+//  Piccy
+//
+//  Created by Jake Torres on 7/6/22.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UserProfileViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Piccy/Piccy/View Controllers/UserProfileViewController.m
@@ -0,0 +1,35 @@
+//
+//  UserProfileViewController.m
+//  Piccy
+//
+//  Created by Jake Torres on 7/6/22.
+//
+
+#import "UserProfileViewController.h"
+
+@interface UserProfileViewController ()
+
+@end
+
+@implementation UserProfileViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (IBAction)backButtonPressed:(id)sender {
+    [self dismissViewControllerAnimated:true completion:nil];
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
