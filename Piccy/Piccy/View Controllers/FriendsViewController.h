d7aa7a48b34cc94e0b2dd0e75be282b043371d1e
Jake Torres <jakert2244@gmail.com>
2022-07-07 14:50:55 -0700

    Added friends screen

---
+++ Piccy/Piccy/View Controllers/FriendsViewController.h
@@ -0,0 +1,16 @@
+//
+//  FriendsViewController.h
+//  Piccy
+//
+//  Created by Jake Torres on 7/7/22.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FriendsViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Piccy/Piccy/View Controllers/FriendsViewController.m
@@ -0,0 +1,31 @@
+//
+//  FriendsViewController.m
+//  Piccy
+//
+//  Created by Jake Torres on 7/7/22.
+//
+
+#import "FriendsViewController.h"
+
+@interface FriendsViewController ()
+
+@end
+
+@implementation FriendsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
