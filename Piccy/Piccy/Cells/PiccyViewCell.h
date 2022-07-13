e2b638d2d0db1201af2efb8c68cc3e500ecf4376
Jake Torres <jakert2244@gmail.com>
2022-07-13 14:32:09 -0700

    Added table view for posts on home screen

---
+++ Piccy/Piccy/Cells/PiccyViewCell.h
@@ -0,0 +1,16 @@
+//
+//  PiccyViewCell.h
+//  Piccy
+//
+//  Created by Jake Torres on 7/13/22.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PiccyViewCell : UITableViewCell
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Piccy/Piccy/Cells/PiccyViewCell.m
@@ -0,0 +1,23 @@
+//
+//  PiccyViewCell.m
+//  Piccy
+//
+//  Created by Jake Torres on 7/13/22.
+//
+
+#import "PiccyViewCell.h"
+
+@implementation PiccyViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
